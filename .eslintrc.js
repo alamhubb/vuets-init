8da1b0a9f7a7a41f87e003e0d57750633f8db6fa
qinkaiyuan <qinkaiyuan94@163.com>
2020-09-18 17:03:11 +0800

    初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,13 +6,27 @@ module.exports = {
   'extends': [
     'plugin:vue/essential',
     'eslint:recommended',
+    '@vue/standard',
     '@vue/typescript/recommended'
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': 0,
+    'spaced-comment': 0,
+    'import/first': 0,
+    //可以throw字符串
+    'no-throw-literal': 0,
+    'no-multiple-empty-lines': 0,
+    'lines-between-class-members': 0,
+    //reject可以不传递错误
+    'prefer-promise-reject-errors': 0,
+    '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/no-inferrable-types': 0
   }
 }
